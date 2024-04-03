1de5a9bd27aa57f51889df131080d129c5e68c5d
Addison Chan <addisonbchan@gmail.com>
2024-04-03 15:09:50 -0700

    huh?

---
+++ game/src/main/java/com/sleepamos/game/gui/screen/CreditsScreen.java
@@ -0,0 +1,27 @@
+package com.sleepamos.game.gui.screen;
+
+import com.simsilica.lemur.Container;
+import com.simsilica.lemur.Label;
+
+public class CreditsScreen extends EscapableScreen {
+    @Override
+    protected void initialize() {
+        Container window = this.createAndAttachContainer();
+
+        window.setLocalTranslation(30, this.getScreenHeight(), 0);
+        window.scale(1.75f);
+
+        window.addChild(new Label("""
+                Made by:
+                Addison Chan
+                Soham Dev
+                Ayushi Mehrotra
+                Stanley Wang
+                
+                APCSA Final Project 2023-24
+                
+                
+                Built with JMonkeyEngine and the Lemur GUI Library for JME.
+                """));
+    }
+}
---
+++ game/src/main/java/com/sleepamos/game/gui/screen/EscapableScreen.java
@@ -0,0 +1,7 @@
+package com.sleepamos.game.gui.screen;
+
+public abstract class EscapableScreen extends Screen {
+    protected EscapableScreen() {
+        super(true);
+    }
+}
