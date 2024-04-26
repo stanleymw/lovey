f2045123b287ab738e7ca929838fdab6dbd0bc94
ebay-kid <72238524+ebay-kid@users.noreply.github.com>
2024-04-26 22:02:49 +0000

    begin defining file format

--- game/src/main/java/com/sleepamos/game/util/LoveySerializable.java
+++ game/src/main/java/com/sleepamos/game/util/LoveySerializable.java
@@ -5,6 +5,35 @@ import java.util.List;
 
 /**
  * A custom serialization interface that is able to handle file versioning smoothly.
+ * 
+ * Ex:
+ * Given a class
+ * {@snippet lang = "java":
+ * public class A implements LoveySerializable {
+ *     public static final byte VERSION = 10;
+ * 
+ *     private double d;
+ *     private boolean bool;
+ *     private B b; // B implements LoveySerializable
+ *     private transient C c; // C does not implement LoveySerializable
+ *     private transient B anotherB; // B implements LoveySerializable
+ *     private C c; // this would issue a warning on serialize if uncommented since C does not implement LoveySerializable. C would not be included in the final serialization.
+ * 
+ *     // Class continues...
+ * }
+ * }
+ * The format of the serialized file would be the following without line breaks or information in parentheses:
+ * type A: (A must be of the root type being serialized, any following class structure definitions after A are for included subelements)
+ * version VERSION (the value, stored as a byte) (note: this value will NOT be read from the class; instead it must be passed in to the serializer)
+ * double;
+ * boolean;
+ * val B;
+ * fin;
+ * type B:
+ * version VERSION (the value)
+ * ... and the variables of B, same format as A.
+ * fin;
+ * 
  */
 public interface LoveySerializable {
     default LoveySerializedComponent toSerializedComponent() {
