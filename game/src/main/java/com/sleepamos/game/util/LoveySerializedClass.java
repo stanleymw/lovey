ce31a4bb2f5b3a6c74f5cacc39625ce4e65f7c75
Addison Chan <addisonbchan@gmail.com>
2024-05-02 15:30:47 -0700

    store super class

--- game/src/main/java/com/sleepamos/game/util/LoveySerializedClass.java
+++ game/src/main/java/com/sleepamos/game/util/LoveySerializedClass.java
@@ -5,37 +5,36 @@ import com.sleepamos.game.util.annotations.LoveySerializableValue;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.sql.Ref;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoveySerializedClass implements Serializable {
-    private static final LoveySerializedClass ROOT = new LoveySerializedClass((byte) 0, null, new HashMap<>());
-
     private final byte version;
 
     private final LoveySerializedClass superclass;
+    private final Class<?> storedClazz;
     private final Map<String, Object> data;
 
-    private LoveySerializedClass(byte version, LoveySerializedClass superclass, Map<String, Object> data) {
+
+    private LoveySerializedClass(byte version, LoveySerializedClass superclass, Class<?> storedClazz, Map<String, Object> data) {
         this.version = version;
         this.superclass = superclass;
+        this.storedClazz = storedClazz;
         this.data = data;
     }
 
-    private LoveySerializedClass(byte version, Class<?> superClazz, Map<String, Object> data, LoveySerializable obj) {
-        this(version, createIfLegalClass(superClazz, obj), data);
+    private LoveySerializedClass(byte version, Class<?> superClazz, Class<?> storedClazz, Map<String, Object> data, LoveySerializable obj) {
+        this(version, createIfLegalClass(superClazz, obj), storedClazz, data);
     }
 
     public LoveySerializedClass(LoveySerializable obj) {
-        this(getClassVersion(obj.getClass()), obj.getClass().getSuperclass(), getObjectData(obj.getClass(), obj), obj);
+        this(getClassVersion(obj.getClass()), obj.getClass().getSuperclass(), obj.getClass(), getObjectData(obj.getClass(), obj), obj);
     }
 
     private static byte getClassVersion(Class<?> clazz) {
         try {
             for(Field f : clazz.getDeclaredFields()) {
-                if(f.getAnnotation(LoveySerializableClassVersion.class) != null) {
+                if(ReflectionUtil.hasAnnotation(f, LoveySerializableClassVersion.class)) {
                     f.setAccessible(true);
                     return f.getByte(null);
                 }
@@ -50,20 +49,32 @@ public class LoveySerializedClass implements Serializable {
         Field[] classFields = clazz.getDeclaredFields();
         Map<String, Object> variableNameToValue = new HashMap<>(classFields.length);
 
-        for(Field f : classFields) {
-            if(!ReflectionUtil.isStatic(f) && ReflectionUtil.hasAnnotation(f, LoveySerializableValue.class)) {
-
+        try {
+            for (Field f : classFields) {
+                f.setAccessible(true);
+                if (!ReflectionUtil.isStatic(f)) {
+                    LoveySerializableValue annotation = f.getAnnotation(LoveySerializableValue.class);
+                    if (annotation != null) {
+                        variableNameToValue.put(annotation.value(), f.get(obj));
+                    }
+                }
             }
+        } catch(Exception e) {
+            throw new NonFatalException("Failed to get object data from class def " + clazz.getName() + " during serialization", e);
         }
 
         return variableNameToValue;
     }
 
+    private static LoveySerializedClass generateStoredRootObject(Class<?> rootClazz) {
+        return new LoveySerializedClass((byte) 0, null, rootClazz, new HashMap<>());
+    }
+
     private static LoveySerializedClass createIfLegalClass(Class<?> clazz, LoveySerializable obj) {
         if(LoveySerializable.class.isAssignableFrom(clazz)) {
-            return new LoveySerializedClass(getClassVersion(clazz), clazz.getSuperclass(), getObjectData(clazz, obj), obj);
+            return new LoveySerializedClass(getClassVersion(clazz), clazz.getSuperclass(), clazz, getObjectData(clazz, obj), obj);
         }
 
-        return ROOT;
+        return generateStoredRootObject(clazz.getSuperclass());
     }
 }
--- game/src/main/java/com/sleepamos/game/util/annotations/LoveySerializableClassVersion.java
+++ game/src/main/java/com/sleepamos/game/util/annotations/LoveySerializableClassVersion.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * Indicates the class version for a LoveySerializable object.
- * This value should be static.
+ * This value should be static. Only one variable should be annotated with this in a single class definition (subclasses may define their own value). Multiple annotations will result in undefined behavior.
  */
 @Documented
 @Target(ElementType.FIELD)
