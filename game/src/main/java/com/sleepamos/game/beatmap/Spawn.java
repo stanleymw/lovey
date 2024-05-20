891c7beee4f50b2761ffed18613e7289b6d7a930
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-05-19 17:37:41 -0700

    improve spawnrecord

--- game/src/main/java/com/sleepamos/game/beatmap/Spawn.java
+++ game/src/main/java/com/sleepamos/game/beatmap/Spawn.java
@@ -4,7 +4,7 @@ import com.sleepamos.game.interactables.Interactable;
 import com.sleepamos.game.serializer.annotations.LoveySerializableClassVersion;
 import com.sleepamos.game.serializer.LoveySerializable;
 
-public record Spawn(Interactable interactable, double time, double speed) implements LoveySerializable {
+public record Spawn(Interactable interactable, double hitTime, double reactionTime) implements LoveySerializable {
     @LoveySerializableClassVersion
     private static final byte VERSION = 10;
 }
