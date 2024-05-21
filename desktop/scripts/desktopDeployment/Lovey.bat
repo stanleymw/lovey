d47efc80857ebff4d3f8e9ec1c2fbc6a36bcbabc
Addison Chan <addisonbchan@gmail.com>
2024-05-20 19:34:13 -0700

    fix deployment issues

--- desktop/scripts/desktopDeployment/Lovey.bat
+++ desktop/scripts/desktopDeployment/Lovey.bat
@@ -1,2 +1,2 @@
-"jre/bin/java" -XX:MaxRAMPercentage=60 -classpath "lib/*" com.sleepamos.desktopmodule.DesktopLauncher
+"jre/bin/java" -XX:MaxRAMPercentage=60 -classpath "../lib/*" com.sleepamos.desktopmodule.DesktopLauncher
 exit 0
